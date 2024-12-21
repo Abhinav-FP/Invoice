ccee48043489a5780e6d67a3e63b1bf05ceb1319
Abhinav-FP <a.mathur@futureprofilez.com>
2024-12-21 17:22:20 +0530

    Added files for missing dependency

---
+++ src/pages/api/hi.ts
@@ -0,0 +1,41 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { generatePdfFromHtml } from "../../utils/pdfGenerator";
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const execPromise = promisify(exec);
+
+type Data = {
+  message: string;
+  status: boolean;
+  pdf: Buffer | null;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    // Install dependencies
+    await execPromise('bash install-deps.sh');
+
+    const data = req.body;
+    console.log('data', data?.html);
+    const pdf = await generatePdfFromHtml(data?.html);
+    console.log('pdf', pdf);
+
+    res.status(200).json({
+      status: true,
+      message: 'PDF generation successful',
+      pdf: pdf,
+    });
+  } catch (error: unknown) {
+    console.log('error', error);
+    res.status(500).json({
+      status: false,
+      message: 'PDF generation Failed. Check vercel logs',
+      pdf: null,
+    });
+  }
+}
