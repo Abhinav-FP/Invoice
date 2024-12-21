2b068e6f059a030060fc5e7dadefe0372464f37a
Abhinav-FP <a.mathur@futureprofilez.com>
2024-12-21 18:02:33 +0530

    Removed vercel.json

--- src/pages/api/hi.ts
+++ src/pages/api/hi.ts
@@ -1,8 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { exec } from "child_process";
+import { promisify } from "util";
 import { generatePdfFromHtml } from "../../utils/pdfGenerator";
-import { exec } from 'child_process';
-import { promisify } from 'util';
 
 const execPromise = promisify(exec);
 
@@ -17,24 +16,26 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   try {
-    // Install dependencies
-    await execPromise('bash install-deps.sh');
+    // Install dependencies only if running on Vercel
+    if (process.env.VERCEL) {
+      await execPromise("bash install-deps.sh");
+    }
 
     const data = req.body;
-    console.log('data', data?.html);
+    console.log("data", data?.html);
     const pdf = await generatePdfFromHtml(data?.html);
-    console.log('pdf', pdf);
+    console.log("pdf", pdf);
 
     res.status(200).json({
       status: true,
-      message: 'PDF generation successful',
+      message: "PDF generation successful",
       pdf: pdf,
     });
   } catch (error: unknown) {
-    console.log('error', error);
+    console.log("error", error);
     res.status(500).json({
       status: false,
-      message: 'PDF generation Failed. Check vercel logs',
+      message: "PDF generation Failed. Check vercel logs",
       pdf: null,
     });
   }
